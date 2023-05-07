0070832e53c34c8b0e0656489e82499bac9d3ae3
faresfares15 <lf_aoufar@esi.dz>
2023-05-07 15:36:54 +0100

    initial commits

---
+++ src/main/java/esi/tp_poo_final/HelloController.java
@@ -0,0 +1,14 @@
+package esi.tp_poo_final;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module esi.tp_poo_final {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens esi.tp_poo_final to javafx.fxml;
+    exports esi.tp_poo_final;
+}
