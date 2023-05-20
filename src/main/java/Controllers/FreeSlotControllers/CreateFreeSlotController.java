1aeeceee572ed91edb028db5656e5fe3e4e23bf2
faresfares15 <lf_aoufar@esi.dz>
2023-05-20 16:16:50 +0100

    merging freeSlots up and running

--- src/main/java/Controllers/FreeSlotControllers/CreateFreeSlotController.java
+++ src/main/java/Controllers/FreeSlotControllers/CreateFreeSlotController.java
@@ -1,7 +1,11 @@
 package Controllers.FreeSlotControllers;
 
+import Exceptions.DayDoesNotHaveFreeSlotsException;
 import Exceptions.EmptyRequiredFieldException;
+import Exceptions.FreeSlotNotFoundException;
+import Models.FreeSlot.FreeSlotSchema;
 import esi.tp_poo_final.HelloApplication;
+import javafx.css.Stylesheet;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -30,6 +34,7 @@ public class CreateFreeSlotController {
     Spinner<Integer> durationMinutesSpinner;
     @FXML
     DatePicker datePicker;
+
     @FXML
     public void initialize() {
         //This method will be called when the view is loaded
@@ -61,72 +66,104 @@ public class CreateFreeSlotController {
         datePicker.setValue(LocalDate.now().plusDays(1));
     }
 
-    //internal class to abstract inputs from methods
-    private class ViewInfos {
-        LocalTime getStartTime() {
-            return LocalTime.of(startTimeHoursSpinner.getValue(), startTimeMinutesSpinner.getValue());
-        }
-
-        public Duration getDuration() throws EmptyRequiredFieldException{
-            //check if an input is not provided
-            if(durationHoursSpinner.getValue() == null && durationMinutesSpinner.getValue() == null) throw new EmptyRequiredFieldException("Duration is required");
-            return Duration.ofHours(durationHoursSpinner.getValue()).plusMinutes(durationMinutesSpinner.getValue());
-        }
-        public LocalDate getDate() throws EmptyRequiredFieldException {
-            //check if an input is not provided
-            if(datePicker.getValue() == null) throw new EmptyRequiredFieldException("Date is required");
-            return datePicker.getValue();
-        }
-    }
-    public void createFreeSlot(){
-        try{
+    public void createFreeSlot() {
+        try {
             //get the inputs from the view
             ViewInfos infos = new ViewInfos();
             LocalDate date = infos.getDate();
             LocalTime startTime = infos.getStartTime();
             Duration duration = infos.getDuration();
             LocalTime endTime = startTime.plus(duration);
+            boolean freeSlotCreated = false;
 
-            if(date.isBefore(LocalDate.now())) throw new Exception("Date cannot be in the past");
+            if (date.isBefore(LocalDate.now())) throw new Exception("Date cannot be in the past");
 
-            if(date.isEqual(LocalDate.now()) && startTime.isBefore(LocalTime.now())) throw new Exception("Start time cannot be in the past");
+            if (date.isEqual(LocalDate.now()) && startTime.isBefore(LocalTime.now()))
+                throw new Exception("Start time cannot be in the past");
 
-            if(duration.toMinutes() == 0) throw new Exception("Duration cannot be 0");
+            if (duration.toMinutes() == 0) throw new Exception("Duration cannot be 0");
 
 
-            try{
-                HelloApplication.freeSlotModel.findMany(date).forEach(freeSlot -> {
-                    if (freeSlot.getStartTime().isBefore(startTime) && freeSlot.getEndTime().isAfter(startTime)) {
+            try {
+                for (FreeSlotSchema freeSlot : HelloApplication.freeSlotModel.findMany(date)) {
+                    if (freeSlot.getStartTime().isBefore(startTime) && freeSlot.getEndTime().isAfter(endTime)
+                            && freeSlot.getStartTime().isBefore(endTime) && freeSlot.getEndTime().isAfter(startTime)) {
                         //new start time is inside the free slot
-                        throw new RuntimeException("Free slot cannot be created");
+//                        HelloApplication.freeSlotModel.update(date, freeSlot.getStartTime(),)
+                        throw new RuntimeException("There is already a free slot in this time zone");
                     }
                     if (freeSlot.getStartTime().isBefore(endTime) && freeSlot.getEndTime().isAfter(endTime)) {
                         //new end time is inside the free slot
-                        throw new RuntimeException("Free slot cannot be created");
+                        if (freeSlot.getStartTime().isBefore(startTime) && freeSlot.getEndTime().isAfter(startTime)) {
+                            throw new RuntimeException("There is already a free slot in this time zone");
+                        } else {
+                            HelloApplication.freeSlotModel.update(date, freeSlot.getStartTime(), startTime);
+                            freeSlotCreated = true;
+
+                        }
+                        //To extend the already existing one
                     }
-                    if (freeSlot.getStartTime().isAfter(startTime) && freeSlot.getEndTime().isBefore(endTime)) {
-                        //new free slot is inside the free slot
-                        throw new RuntimeException("Free slot cannot be created");
+                    if (freeSlot.getStartTime().isAfter(startTime) && freeSlot.getEndTime().isBefore(endTime) &&
+                    freeSlot.getEndTime().isAfter(startTime) && freeSlot.getStartTime().isBefore(endTime)) {
+                        // free slot is inside the new free slot
+                        //a new larger free slot will be created
+                        HelloApplication.freeSlotModel.delete(date, freeSlot.getStartTime());
+                        HelloApplication.freeSlotModel.create(date, startTime, endTime);
+                        freeSlotCreated = true;
+
                     }
-                });
-            }catch (Exception e){
-                //do nothing, no free slots found to compare with
+                    if (freeSlot.getStartTime().isBefore(startTime) && freeSlot.getEndTime().isAfter(startTime)) {
+                        if (freeSlot.getEndTime().isBefore(endTime) && freeSlot.getStartTime().isBefore(endTime) && freeSlot.getEndTime().isAfter(startTime)) {
+                            //extend the already existing one
+                            HelloApplication.freeSlotModel.delete(date, freeSlot.getStartTime());
+                            HelloApplication.freeSlotModel.create(date, freeSlot.getStartTime(), endTime);
+                            freeSlotCreated = true;
+
+                        } else throw new RuntimeException("There is already a free slot in this time zone");
+                    }
+
+                    //New logic
+//                    if (freeSlot.getStartTime().isBefore(startTime)) {
+//                        if (freeSlot.getEndTime().isAfter(endTime)) {
+//                            throw new RuntimeException("There is already a free slot in this time zone");
+//                        }
+//                    }
+
+
+                }
+                if (!freeSlotCreated) {
+                    HelloApplication.freeSlotModel.create(date, startTime, endTime);
+                    freeSlotCreated = true;
+                }
+            } catch (DayDoesNotHaveFreeSlotsException e) {
+                //no free slots found to compare with so create it directly
+                HelloApplication.freeSlotModel.create(date, startTime, endTime);
+                freeSlotCreated = true;
+
+            } catch (Exception e) {
+
             }
 
-            if(!confirmSchedule("Free slot schedule confirmation","Free slot can be scheduled in the date and time. Do you confirm the operation?")){
-                System.out.println("Operation cancelled");
-                return;
+            if (freeSlotCreated) {
+                if (!confirmSchedule("Free slot schedule confirmation", "Free slot can be scheduled in the date and time. Do you confirm the operation?")) {
+                    System.out.println("Operation cancelled");
+                    return;
+                }
+                showSuccessMessage("Free slot created successfully");
+            } else {
+                showErrorMessage("Free slot couldn't be created");
             }
 
-            //create the free slot
-            HelloApplication.freeSlotModel.create(infos.getDate(), infos.getStartTime(), infos.getStartTime().plus(infos.getDuration()));
+//            //create the free slot
+//            HelloApplication.freeSlotModel.create(infos.getDate(), infos.getStartTime(), infos.getStartTime().plus(infos.getDuration()));
+//
 
-            showSuccessMessage("Free slot created successfully");
 
-        }catch (Exception e){
+        } catch (Exception e) {
             showErrorMessage(e.getMessage());
         }
     }
+
     public void moveToCalendarView() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calendar-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 840, 400);
@@ -142,7 +179,7 @@ public class CreateFreeSlotController {
     }
 
     //helper methods
-    private boolean confirmSchedule(String title, String message){
+    private boolean confirmSchedule(String title, String message) {
         Alert confirmationMessage = new Alert(Alert.AlertType.CONFIRMATION);
         confirmationMessage.setContentText(message);
         confirmationMessage.setHeaderText(title);
@@ -151,7 +188,8 @@ public class CreateFreeSlotController {
 
         return clickedButton.get() == ButtonType.OK;
     }
-    private void showErrorMessage(String message){
+
+    private void showErrorMessage(String message) {
         Alert errorMessage = new Alert(Alert.AlertType.ERROR);
         errorMessage.setContentText(message);
         errorMessage.setHeaderText("Error");
@@ -159,11 +197,31 @@ public class CreateFreeSlotController {
         errorMessage.showAndWait();
     }
 
-    private void showSuccessMessage(String message){
+    private void showSuccessMessage(String message) {
         Alert successMessage = new Alert(Alert.AlertType.INFORMATION);
         successMessage.setContentText(message);
         successMessage.setHeaderText("Success");
         successMessage.setTitle("Success");
         successMessage.showAndWait();
+    }
+
+    //internal class to abstract inputs from methods
+    private class ViewInfos {
+        LocalTime getStartTime() {
+            return LocalTime.of(startTimeHoursSpinner.getValue(), startTimeMinutesSpinner.getValue());
+        }
+
+        public Duration getDuration() throws EmptyRequiredFieldException {
+            //check if an input is not provided
+            if (durationHoursSpinner.getValue() == null && durationMinutesSpinner.getValue() == null)
+                throw new EmptyRequiredFieldException("Duration is required");
+            return Duration.ofHours(durationHoursSpinner.getValue()).plusMinutes(durationMinutesSpinner.getValue());
+        }
+
+        public LocalDate getDate() throws EmptyRequiredFieldException {
+            //check if an input is not provided
+            if (datePicker.getValue() == null) throw new EmptyRequiredFieldException("Date is required");
+            return datePicker.getValue();
+        }
     }
 }
