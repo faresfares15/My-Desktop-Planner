e4dbfc94f9d374f3ead1e3bbaed9732cbb2b142d
faresfares15 <lf_aoufar@esi.dz>
2023-05-21 21:14:48 +0100

    changeSEttings

---
+++ src/main/java/Controllers/UserControllers/ChangeSettingsController.java
@@ -0,0 +1,15 @@
+package Controllers.UserControllers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+
+public class ChangeSettingsController  implements EventHandler<ActionEvent> {
+
+    @FXML
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
+}
